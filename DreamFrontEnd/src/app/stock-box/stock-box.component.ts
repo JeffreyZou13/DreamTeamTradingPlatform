7f634e483c0b0ad4bbb18ded0bb740c17636641a
mem2318 <mem2318@columbia.edu>
2019-07-28 09:45:59 -0400

    added js to exit a module

--- DreamFrontEnd/src/app/stock-box/stock-box.component.ts
+++ DreamFrontEnd/src/app/stock-box/stock-box.component.ts
@@ -13,9 +13,19 @@ import * as $ from 'jquery';
 })
 
 export class StockBoxComponent implements OnInit {
-
+  
+  // show:boolean = true;
   changeLabel(obj) {
     (<HTMLInputElement>document.getElementById("strategySelector")).innerHTML = obj;
+  }
+
+  addModule(obj){
+    
+  }
+
+  exitModule(obj){
+    (<HTMLInputElement>document.getElementById("module1")).remove();
+    //WILL HAVE TO ADD LOGIC TO EXIT TRADE HERE, ALSO WITH ALERT
   }
 
   constructor() {
