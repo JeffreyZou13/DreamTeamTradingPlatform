5e1423fcfd16d33867509fd9bd90846bdd0edb9a
mem2318 <mem2318@columbia.edu>
2019-07-29 16:07:13 -0400

    working on dynamically adding html with event listeners, thank u tony

--- DreamFrontEnd/src/app/stock-box/stock-box.component.ts
+++ DreamFrontEnd/src/app/stock-box/stock-box.component.ts
@@ -23,10 +23,16 @@ export class StockBoxComponent implements OnInit {
   }
 
   addModule(obj){
-    var copy = document.getElementById("module1");
-    var clone = copy.cloneNode(true);
-    // clone.id = "module2"
-    document.getElementById("top-level").appendChild(clone);
+    // var copy = document.getElementById("module1");
+    // var clone = copy.cloneNode(true);
+    // document.getElementById("top-level").appendChild(clone);
+
+        // $("#bt3").click(
+        //     function () {
+        //       console.log('this was fired')
+                
+        //     }
+        // )
 
   }
 
@@ -40,6 +46,24 @@ export class StockBoxComponent implements OnInit {
   }
 
   ngOnInit() {
+    var stratCounter = 0;
+        $("#bt3").click(
+            function () {
+                stratCounter++;
+                var markup = `<div id="stratBox${stratCounter}" class="stratBox" > <button id="redButton${stratCounter}" class="redButtonClass">red button</button> <input type="text" id="redText${stratCounter}" class="redTextClass"  value=""/>  </div>`;
+                $("body").append(markup);
+
+                $(".redButtonClass").click(
+                    function () {
+                        var id = $(this).attr("id");
+                        var idLength = id.length;
+                        var strNumber = id.substr(idLength - 1, 1);
+                        $("#redText" + strNumber).val("I was clicked");
+                        $("#topDiv").text("A click occurred in stratBox numbered: " + strNumber);
+                    }
+                )
+            }
+        )
     
   }
 
