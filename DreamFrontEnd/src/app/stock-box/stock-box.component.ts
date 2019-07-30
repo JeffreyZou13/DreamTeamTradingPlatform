b5381a11501c2e002118db9a4cb02ec3e7752955
mem2318 <mem2318@columbia.edu>
2019-07-30 18:01:57 -0400

    refactored front end, i think this is working

--- DreamFrontEnd/src/app/stock-box/stock-box.component.ts
+++ DreamFrontEnd/src/app/stock-box/stock-box.component.ts
@@ -14,9 +14,9 @@ import * as $ from 'jquery';
 })
 
 export class StockBoxComponent implements OnInit {
-
-
-  stockboxes: Array<StockBoxComponent> = [];
+  showTradesPage:boolean = true;
+  stratCounter:number = 0;
+  addEles:boolean = false;
 
   changeLabel(obj1 ,obj2) {
     (<HTMLInputElement>document.getElementById(obj1)).innerHTML = obj2;
@@ -27,34 +27,33 @@ export class StockBoxComponent implements OnInit {
     //WILL HAVE TO ADD LOGIC TO EXIT TRADE HERE, ALSO WITH ALERT
   }
 
-  handlePause(obj){
-
+  hideTrades(){
+    this.showTradesPage = false;
+    document.getElementById("menu2").classList.add("active");
+    document.getElementById("menu1").classList.remove("active");
   }
 
-  constructor() {
-
+  showTrades(){
+    this.showTradesPage = true;
+    document.getElementById("menu2").classList.remove("active");
+    document.getElementById("menu1").classList.add("active")
   }
 
+  toggleTradePage(){
+    this.showTradesPage = !this.showTradesPage;
+  }
 
-  ngOnInit() {
+  buttonThreeClick(){
+    // var stratCounter = 0;
+    // var addEles = false;
 
-    $("#bt3").click(
-      function () {
-      }
-
-    );
-
-    var stratCounter = 0;
-    var addEles = false;
-    $("#bt3").click(
-      function () {
-        stratCounter++;
+    this.stratCounter++;
         var stockName = (<HTMLInputElement>document.getElementById("stockSelector")).value;
         var stockQuantity = (<HTMLInputElement>document.getElementById("quantSelector")).value;
         var strat = (<HTMLInputElement>document.getElementById("strategySelector")).innerHTML;
-        var stratId = "strat" + stratCounter;
-        var yId = "y" + stratCounter;
-        var gId = "g" + stratCounter;
+        var stratId = "strat" + this.stratCounter;
+        var yId = "y" + this.stratCounter;
+        var gId = "g" + this.stratCounter;
 
         var postObj = {
           "type":"two moving averages",
@@ -68,7 +67,7 @@ export class StockBoxComponent implements OnInit {
           || strat == "Select Strategy" ){
           alert("All fields are required")
         }else{
-          addEles = true;
+          this.addEles = true;
         }
 
         var markup =
@@ -80,43 +79,43 @@ export class StockBoxComponent implements OnInit {
             <td>1</td>
             <td>good</td>
             <td>
-              <button type="button" class="btn btn-success" style="margin:0px" id=${stratCounter}>Start</button>
-              <button type="button" class="btn btn-warning" style="margin:0px" id=${stratCounter}>Pause</button>
-              <button type="button" class="btn btn-danger" style="margin:0px" id=${stratCounter}>Exit</button>
+              <button type="button" class="btn btn-success" style="margin:0px" id=${this.stratCounter}>Start</button>
+              <button type="button" class="btn btn-warning" style="margin:0px" id=${this.stratCounter}>Pause</button>
+              <button type="button" class="btn btn-danger" style="margin:0px" id=${this.stratCounter}>Exit</button>
             </td>
           </tr>
         </tbody>`
 
-        if(addEles){
+        if(this.addEles){
           console.log("ehehehe")
           document.getElementById("t1").style["display"] = "";
           $("#t1").append(markup);
 
           $(".btn-danger").click(
             function () {
-                var id = $(this).attr("id");
-                var remove;
-                remove = "#strat" + id;
-                $(remove).remove();
+              var id = $(this).attr("id");
+              var remove;
+              remove = "#strat" + id;
+              $(remove).remove();
             }
           )
 
           $(".btn-warning").click(
             function () {
-                console.log('he')
-                var id = $(this).attr("id");
-                var remove;
-                remove = "#strat" + id;
-                $(remove).css("background","#feffd4");
+              console.log('he')
+              var id = $(this).attr("id");
+              var remove;
+              remove = "#strat" + id;
+              $(remove).css("background","#feffd4");
             }
           )
 
           $(".btn-success").click(
             function () {
-                var id = $(this).attr("id");
-                var remove;
-                remove = "#strat" + id;
-                $(remove).css("background","#e1f5e6");
+              var id = $(this).attr("id");
+              var remove;
+              remove = "#strat" + id;
+              $(remove).css("background","#e1f5e6");
             }
           )
         }
@@ -130,6 +129,103 @@ export class StockBoxComponent implements OnInit {
             console.log("yayy made it here")
           }
         });
+
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+    var stratCounter = 0;
+    var addEles = false;
+
+    $("#bt3").click(
+      function () {
+
+        // stratCounter++;
+        // var stockName = (<HTMLInputElement>document.getElementById("stockSelector")).value;
+        // var stockQuantity = (<HTMLInputElement>document.getElementById("quantSelector")).value;
+        // var strat = (<HTMLInputElement>document.getElementById("strategySelector")).innerHTML;
+        // var stratId = "strat" + stratCounter;
+        // var yId = "y" + stratCounter;
+        // var gId = "g" + stratCounter;
+
+        // var postObj = {
+        //   "type":"two moving averages",
+        //   "stock": stockName,
+        //   "shortPeriod": 1,
+        //   "longPeriod":2,
+        //   "size":stockQuantity
+        // }
+
+        // if(stockName == "" || stockQuantity == ""
+        //   || strat == "Select Strategy" ){
+        //   alert("All fields are required")
+        // }else{
+        //   addEles = true;
+        // }
+
+        // var markup =
+        // `<tbody id=${stratId} style="background:#e1f5e6">
+        //   <tr>
+        //     <td>${stockName}</td>
+        //     <td>${stockQuantity}</td>
+        //     <td>${strat}</td>
+        //     <td>1</td>
+        //     <td>good</td>
+        //     <td>
+        //       <button type="button" class="btn btn-success" style="margin:0px" id=${stratCounter}>Start</button>
+        //       <button type="button" class="btn btn-warning" style="margin:0px" id=${stratCounter}>Pause</button>
+        //       <button type="button" class="btn btn-danger" style="margin:0px" id=${stratCounter}>Exit</button>
+        //     </td>
+        //   </tr>
+        // </tbody>`
+
+        // if(addEles){
+        //   console.log("ehehehe")
+        //   document.getElementById("t1").style["display"] = "";
+        //   $("#t1").append(markup);
+
+        //   $(".btn-danger").click(
+        //     function () {
+        //       var id = $(this).attr("id");
+        //       var remove;
+        //       remove = "#strat" + id;
+        //       $(remove).remove();
+        //     }
+        //   )
+
+        //   $(".btn-warning").click(
+        //     function () {
+        //       console.log('he')
+        //       var id = $(this).attr("id");
+        //       var remove;
+        //       remove = "#strat" + id;
+        //       $(remove).css("background","#feffd4");
+        //     }
+        //   )
+
+        //   $(".btn-success").click(
+        //     function () {
+        //       var id = $(this).attr("id");
+        //       var remove;
+        //       remove = "#strat" + id;
+        //       $(remove).css("background","#e1f5e6");
+        //     }
+        //   )
+        // }
+
+        // $.ajax({
+        //   type: "POST",
+        //   url: 'http://localhost:8080/strategy/start',
+        //   contentType:"application/json",
+        //   data: JSON.stringify(postObj),
+        //   success: function(response) {
+        //     console.log("yayy made it here")
+        //   }
+        // });
       }
     )
 
