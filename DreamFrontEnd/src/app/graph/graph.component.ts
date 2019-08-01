7b1dfc8d9a8856eb8438136f448d68f0c745ad4e
mem2318 <mem2318@columbia.edu>
2019-08-01 23:46:00 +0000

    my happiest commit, finally graphed profit

--- DreamFrontEnd/src/app/graph/graph.component.ts
+++ DreamFrontEnd/src/app/graph/graph.component.ts
@@ -10,13 +10,6 @@ declare let $:any;
 export class GraphComponent implements OnInit {
   history:any;
 
-  // doThis(){
-  //   console.log('HEY CMOND')
-  // }
-
-   doThis(){
-    console.log('i need help');
-  }
 
   constructor() {
 
@@ -25,61 +18,63 @@ export class GraphComponent implements OnInit {
 
   	ngOnInit() {
 
+      function doThis() {
+        console.log("I AM HERE");
+      }
+
       $.ajax({
         type: "GET",
-        url: 'http://localhost:8081/history/strategies/notstopped',
+        url: 'http://localhost:8081/history/strategies',
         contentType: 'application/json',
         success: (response)=> {
           console.log(response)
 
+
           for (var i = 0; i < response.strategies["bollinger band"].length; i++){
-            var x;
+            var x= response.strategies["bollinger band"][i]['strategyID']
+            // var x = '5e5f2c7f-7122-4a0b-9f7b-627798139204'
             var markup =
-            `<button ngbDropdownItem onclick=
-            "document.getElementById('currentId').innerHTML = '${response.strategies['bollinger band'][i][0]}'">
-            ${response.strategies["bollinger band"][i][0]}
+            `<button ngbDropdownItem onclick="doThis('${x}')">
+            ${response.strategies["bollinger band"][i]['strategyID']}
             </button>`
             $("#performanceSelector2").append(markup);
-
           }
         }
       })
 
-
-
-      $.ajax({
-        type: "GET",
-        url: 'http://localhost:8081/history/orders/5e5f2c7f-7122-4a0b-9f7b-627798139204',
-        contentType:"application/json",
-        success: function(response) {
-          console.log(response)
-          let dataPoints = [];
-        	let y = 0;
-        	for ( var i = 0; i < response.orders.length; i++ ) {
-        		y = response.orders[i].profit
-        		dataPoints.push({ y: y});
-        	}
-
-        	let chart = new CanvasJS.Chart("chartContainer", {
-        		zoomEnabled: true,
-        		animationEnabled: true,
-        		exportEnabled: true,
-        		title: {
-        			text: "Two Moving Averages Example"
-        		},
-        		subtitles:[{
-        			text: "Try Zooming and Panning"
-        		}],
-        		data: [
-        		{
-        			type: "line",
-        			dataPoints: dataPoints
-        		}]
-        	});
-        	chart.render();
-
-        }
-      });
+      // $.ajax({
+      //   type: "GET",
+      //   url: 'http://localhost:8081/history/orders/5e5f2c7f-7122-4a0b-9f7b-627798139204',
+      //   contentType:"application/json",
+      //   success: function(response) {
+      //     console.log(response)
+      //     let dataPoints = [];
+      //   	let y = 0;
+      //   	for ( var i = 0; i < response.orders.length; i++ ) {
+      //   		y = response.orders[i].profit
+      //   		dataPoints.push({ y: y});
+      //   	}
+      //
+      //   	let chart = new CanvasJS.Chart("chartContainer", {
+      //   		zoomEnabled: true,
+      //   		animationEnabled: true,
+      //   		exportEnabled: true,
+      //   		title: {
+      //   			text: "Two Moving Averages Example"
+      //   		},
+      //   		subtitles:[{
+      //   			text: "Try Zooming and Panning"
+      //   		}],
+      //   		data: [
+      //   		{
+      //   			type: "line",
+      //   			dataPoints: dataPoints
+      //   		}]
+      //   	});
+      //   	chart.render();
+      //
+      //   }
+      // });
 
     }
 }
