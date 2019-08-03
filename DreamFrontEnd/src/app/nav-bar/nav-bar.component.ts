85955dfe032c125d1d6fae3436e261fed27882cc
mem2318 <mem2318@columbia.edu>
2019-08-03 17:32:18 +0000

    made table scrollable

--- DreamFrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ DreamFrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -13,8 +13,8 @@ export class NavBarComponent implements OnInit {
     this.showTradesPage = false;
     document.getElementById("graphPage").style.display = "";
     document.getElementById("allTrades").style.display = "none";
-    document.getElementById("chartContainer").style.display = "";
-    document.getElementById("chartContainer2").style.display = "";
+    document.getElementById("chartContainer").style.display = "inline-block";
+    document.getElementById("chartContainer2").style.display = "inline-block";
     document.getElementById("menu2").classList.add("active");
     document.getElementById("menu1").classList.remove("active");
   }
@@ -24,7 +24,7 @@ export class NavBarComponent implements OnInit {
     document.getElementById("graphPage").style.display = "none";
     document.getElementById("allTrades").style.display = "";
     document.getElementById("chartContainer").style.display = "none";
-    document.getElementById("chartContainer2").style.display = "";
+    document.getElementById("chartContainer2").style.display = "none";
     document.getElementById("menu2").classList.remove("active");
     document.getElementById("menu1").classList.add("active")
   }
